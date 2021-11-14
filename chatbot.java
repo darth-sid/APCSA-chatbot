a2ddaa7c0de4831d41edb94e872fa9403b262cbd
darth-sid <55854690+darth-sid@users.noreply.github.com>
2021-11-14 12:55:41 -0800

    transform statements [WIP]

--- chatbot.java
+++ chatbot.java
@@ -19,22 +19,40 @@ public class chatbot
         responseDict.put("greeting", "Hello there!~Hey there!~Deez Nutz!");
         responseDict.put("random","Hmmmm~Interesting, tell me more.");
         responseDict.put("clarify","What does that mean?~Speak more clearly, noob.");
-
+        responseDict.put("transform[want]","Why do you want ~I also want ");
+        responseDict.put("transform[need]","You don't need ~Why do you need ~I do not need ");
+        responseDict.put("reason","yes");
         keywordDict.put("greeting","Hello~Hi~What's Up~Hey");
+        keywordDict.put("transform[want]","I want~I wish");
+        keywordDict.put("transform[need]","I need~I require");
+        keywordDict.put("reason","why");
+
+
     }
-    public static String getMessage(String intent){
+    public String getMessage(String raw){
+        String[] data = raw.split("~");
+        String intent = data[0];
         String responses = responseDict.get(intent);
         String[] responseList = responses.split("~");
+        if (intent.contains("transform")){
+            String keyword = data[1];
+            return responseList[(int) (Math.random() * responseList.length)]+keyword;
+        }
         return responseList[(int) (Math.random() * responseList.length)];
     }
-    public static String getIntent(String sentence){
+    public String getIntent(String sentence){
         String cleanSentence = cleanStatement(sentence);
         Set<String> keys = keywordDict.keySet();
         for (String key : keys){
             String keywords = keywordDict.get(key);
             String[] keywordList = keywords.split("~");
             for (String keyword : keywordList){
-                if (cleanSentence.contains(keyword)) return key;
+                if (cleanSentence.contains(keyword.toLowerCase())){
+                    if(key.contains("transform")){
+                        return key+"~"+keyword(sentence,keyword);
+                    }
+                    return key;
+                }
             }
         }
         return "random";
@@ -50,7 +68,7 @@ public class chatbot
      {
        if (Character.isAlphabetic(sentence.charAt(index)) 
           || sentence.charAt(index) == ' ') {
-         cleanSentence += sentence.substring(index, index + 1);
+         cleanSentence += sentence.substring(index, index + 1).toLowerCase();
        }
      }
      return cleanSentence;
@@ -61,33 +79,44 @@ public class chatbot
     * @param statement the user statement, assumed to contain "I want to"
     * @return the transformed statement
     */
-   private String transformIWantToStatement(String statement)
+   /*private String transformIWantToStatement(String statement)
    {
       statement = cleanStatement(statement);
       int pos = findKeyword (statement, "I want to", 0);
       String restOfStatement = statement.substring(pos + 9).trim();
       return "What would it mean to " + restOfStatement + "?";
-   }
+   }*/
 
    /**  ADD CODE HERE!
     * Take a statement with "I want <something>." and transform it into
     * Would you really be happy if you had <something>?
     * @param statement the user statement, assumed to contain "I want"
     * @return the transformed statement
-    */
+    *//*
    private String transformIWantStatement(String statement)
    {
       // ADD CODE HERE
 
       return "Would you really be happy if you had ...";
+   }*/
+    
+   private static String keyword(String statement, String fragment){
+       if (statement.contains(fragment)){
+            String word = "";
+            int start = statement.indexOf(fragment) + fragment.length()+1;
+            int end = statement.substring(start,statement.length()).indexOf(".");
+            if (end == -1) end = statement.substring(start,statement.length()).length();
+            word = statement.substring(start, start+end);
+            return word;
+        }
+        else return "urmom";
    }
-
-   /**
+      /**
     * Take a statement with "you <something> me" and transform it into
     * "What makes you think that I <something> you?"
     * @param statement the user statement, assumed to contain "you" followed by "me"
     * @return the transformed statement
-    */
+    *//*
    private String transformYouMeStatement(String statement)
    {
       statement = cleanStatement(statement);
@@ -96,19 +125,19 @@ public class chatbot
 
       String restOfStatement = statement.substring(posOfYou + 3, posOfMe).trim();
       return "What makes you think that I " + restOfStatement + " you?";
-   }
+   }*/
 
   /**  ADD THIS
     * Take a statement with "I <something> you" and transform it into
     * "Why do you <something> me?"
     * @param statement the user statement, assumed to contain "I" followed by something "you"
     * @return the transformed statement
-    */
+    *//*
    private String transformIMeStatement(String statement)
    {
      // ADD CODE HERE
      return "Why do you...";
-   }
+   }*/
 
    /**
     * Pick a default response to use if nothing else fits.
@@ -124,12 +153,9 @@ public class chatbot
         
             // Conversation begins
             System.out.println(chatbot.getMessage("greeting"));
-            System.out.println(chatbot.getMessage("clarify"));
-            System.out.println(chatbot.getMessage("random"));
             statement = in.nextLine();
             while (!(statement.equals("|quit"))){
-                // System.out.println(statement);
-                System.out.println("Response: " + chatbot.getMessage(getIntent(statement)));
+                System.out.println("Response: " + chatbot.getMessage(chatbot.getIntent(statement)));
                 statement = in.nextLine();
                 
             }
